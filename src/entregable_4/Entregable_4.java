0b4acaffde2df75f1e0bf040a4eaaa3827bb8640
kebelth <83265694+kebelth@users.noreply.github.com>
2021-12-01 20:02:54 -0600

    Update Entregable_4.java
    menu

--- src/entregable_4/Entregable_4.java
+++ src/entregable_4/Entregable_4.java
@@ -1,25 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package entregable_4;
 
-/**
- *
- * @author Keiren
- */
+import javax.swing.JOptionPane;
 public class Entregable_4 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-      System.out.println("Bienvenido al menu principal de la Biblioteca de Gotham");
+      JOptionPane.showMessageDialog(null,"Bienvenido al menu principal de la Biblioteca de Gotham");
       String Librería;
       String Registro_de_Usuario;
       String Facturas;
       String Salida;
+         
+       boolean salir= false;
+       int opcion;
+       Carnet mostrar1=new Carnet();
+       
+         while(!salir){
+             JOptionPane.showMessageDialog(null,"\n>>> MENÚ DE OPCIONES <<<"
+                + "\n1. "
+                + "\n2.Crear un Carnet de la biblioteca ."
+                +"\n3. ."
+                + "\n4. Salir.");
                 
+            
+             String opcion1   =JOptionPane.showInputDialog("\nIntroduzca opción (1-4):");  
+              opcion=Integer.parseInt(opcion1);
+             
+              switch (opcion){
+                  case 1:
+                      JOptionPane.showMessageDialog(null,"has seleccinado la opcion 1");
+                      
+                      
+                      
+                      
+                      break;
+                   case 2:
+                      JOptionPane.showMessageDialog(null,"has seleccinado la opcion 2");
+                      mostrar1.RegistroCarnet();
+                      
+                      
+                      
+                      break;
+                   case 3:
+                      JOptionPane.showMessageDialog(null,"has seleccinado la opcion 3");
+                      
+                      
+                      
+                      break;
+                   case 4:
+                      JOptionPane.showMessageDialog(null,">>SALIENDO<<");
+                      salir = true;
+                      break;
+                   default :
+                         JOptionPane.showMessageDialog(null," solo numeros entre 1 y 4");
+               }
+              
+             }        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
 }
